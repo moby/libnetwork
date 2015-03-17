3205c598977c4c01a214a5ac05de3840f67bc962
Arnaud Porterie <arnaud.porterie@docker.com>
2015-03-16 19:11:58 -0700

    Add Driver interface
    Signed-off-by: Arnaud Porterie <arnaud.porterie@docker.com>

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -6,17 +6,23 @@ import (
 	"net"
 
 	"github.com/docker/libnetwork"
-	_ "github.com/docker/libnetwork/drivers/bridge"
+	"github.com/docker/libnetwork/drivers/bridge"
 )
 
 func main() {
 	ip, net, _ := net.ParseCIDR("192.168.100.1/24")
 	net.IP = ip
 
-	options := libnetwork.DriverParams{"AddressIPv4": net}
-	netw, err := libnetwork.NewNetwork("simplebridge", "dummy", options)
+	drv, err := libnetwork.NewDriver("simplebridge")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	options := bridge.Configuration{AddressIPv4: net}
+	netw, err := drv.NewNetwork("dummy", options)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Printf("Network=%#v\n", netw)
 }
