64dd4592634ac0fe9a15ebc76b15b7df6faa3239
Chun Chen <ramichen@tencent.com>
2015-12-09 11:47:28 +0800

    Fix DriverOpts option in networkCreate
    Signed-off-by: Chun Chen <ramichen@tencent.com>

--- client/types.go
+++ client/types.go
@@ -34,9 +34,9 @@ type SandboxResource struct {
 
 // networkCreate is the expected body of the "create network" http request message
 type networkCreate struct {
-	Name        string   `json:"name"`
-	NetworkType string   `json:"network_type"`
-	DriverOpts  []string `json:"driver_opts"`
+	Name        string            `json:"name"`
+	NetworkType string            `json:"network_type"`
+	DriverOpts  map[string]string `json:"driver_opts"`
 }
 
 // serviceCreate represents the body of the "publish service" http request message
