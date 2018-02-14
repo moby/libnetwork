f7401b6f2a277bbf5e4967a23dae8b21c46a1439
Chris Telfer <ctelfer@docker.com>
2018-02-14 17:04:23 -0500

    Gracefully remove LB endpoints from services
    This patch attempts to allow endpoints to complete servicing connections
    while being removed from a service.  The change adds a flag to the
    endpoint.deleteServiceInfoFromCluster() method to indicate whether this
    removal should fully remove connectivity through the load balancer
    to the endpoint or should just disable directing further connections to
    the endpoint.  If the flag is 'false', then the load balancer assigns
    a weight of 0 to the endpoint but does not remove it as a linux load
    balancing destination.  It does remove the endpoint as a docker load
    balancing endpoint but tracks it in a special map of "disabled-but-not-
    destroyed" load balancing endpoints.  This allows traffic to continue
    flowing, at least under Linux.  If the flag is 'true', then the code
    removes the endpoint entirely as a load balancing destination.
    
    The sandbox.DisableService() method invokes deleteServiceInfoFromCluster()
    with the flag sent to 'false', while the endpoint.sbLeave() method invokes
    it with the flag set to 'true' to complete the removal on endpoint
    finalization.  Renaming the endpoint invokes deleteServiceInfoFromCluster()
    with the flag set to 'true' because renaming attempts to completely
    remove and then re-add each endpoint service entry.
    
    The controller.rmServiceBinding() method, which carries out the operation,
    similarly gets a new flag for whether to fully remove the endpoint.  If
    the flag is false, it does the job of moving the endpoint from the
    load balancing set to the 'disabled' set.  It then removes or
    de-weights the entry in the OS load balancing table via
    network.rmLBBackend().  It removes the service entirely via said method
    ONLY IF there are no more live or disabled load balancing endpoints.
    Similarly network.addLBBackend() requires slight tweaking to properly
    manage the disabled set.
    
    Finally, this change requires propagating the status of disabled
    service endpoints via the networkDB.  Accordingly, the patch includes
    both code to generate and handle service update messages.  It also
    augments the service structure with a ServiceDisabled boolean to convey
    whether an endpoint should ultimately be removed or just disabled.
    This, naturally, required a rebuild of the protocol buffer code as well.
    
    Signed-off-by: Chris Telfer <ctelfer@docker.com>
    (cherry picked from commit 2af06a083b9710f1a9711934b4c0aa6e0bd136e0)

--- service_windows.go
+++ service_windows.go
@@ -5,7 +5,7 @@ import "net"
 func (n *network) addLBBackend(ip, vip net.IP, fwMark uint32, ingressPorts []*PortConfig) {
 }
 
-func (n *network) rmLBBackend(ip, vip net.IP, fwMark uint32, ingressPorts []*PortConfig, rmService bool) {
+func (n *network) rmLBBackend(ip, vip net.IP, lb *loadBalancer, ingressPorts []*PortConfig, rmService bool, fullRemove bool) {
 }
 
 func (sb *sandbox) populateLoadbalancers(ep *endpoint) {
